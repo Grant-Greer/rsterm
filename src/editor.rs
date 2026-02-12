842a4abd1d6c47df254683e83d6aa72689c17d60
Grant <grantgreerdev@gmail.com>
2026-02-11 17:22:53 -0800

    test

--- src/editor.rs
+++ src/editor.rs
@@ -1,33 +1,136 @@
-use crossterm::event::{Event::Key, KeyCode::Char, read};
-use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
+use core::cmp::min;
+use crossterm::event::{
+    Event::{self, Key},
+    KeyCode, KeyEvent, KeyEventKind, KeyModifiers, read,
+};
 
-pub struct Editor {}
+use std::io::Error;
+mod terminal;
+mod view;
+use terminal::{Position, Size, Terminal};
+
+use view::View;
+
+#[derive(Copy, Clone, Default)]
+struct Location {
+    x: usize,
+    y: usize,
+}
+
+#[derive(Default)]
+pub struct Editor {
+    should_quit: bool,
+    location: Location,
+    view: View,
+}
 
 impl Editor {
-    pub fn default() -> Self {
-        Editor {}
+    pub fn run(&mut self) {
+        Terminal::initialize().unwrap();
+        let result = self.repl();
+        Terminal::terminate().unwrap();
+        result.unwrap();
     }
 
-    pub fn run(&self) {
-        enable_raw_mode().unwrap();
+    fn repl(&mut self) -> Result<(), Error> {
         loop {
-            match read() {
-                Ok(Key(event)) => {
-                    println!("{:?} \r", event);
-                    match event.code {
-                        Char(c) => {
-                            if c == 'q' {
-                                break;
-                            }
-                        }
-
-                        _ => (),
-                    }
-                }
-                Err(err) => println!("Error: {}", err),
-                _ => (),
+            self.refresh_screen()?;
+            if self.should_quit {
+                break;
             }
+            let event = read()?;
+            self.evaluate_event(event)?;
         }
-        disable_raw_mode().unwrap();
+        Ok(())
+    }
+    fn move_point(&mut self, key_code: KeyCode) -> Result<(), Error> {
+        let Location { mut x, mut y } = self.location;
+        let Size { height, width } = Terminal::size()?;
+        match key_code {
+            KeyCode::Up => {
+                y = y.saturating_sub(1);
+            }
+            KeyCode::Down => {
+                y = min(height.saturating_sub(1), y.saturating_add(1));
+            }
+            KeyCode::Left => {
+                x = x.saturating_sub(1);
+            }
+            KeyCode::Right => {
+                x = min(width.saturating_sub(1), x.saturating_add(1));
+            }
+            KeyCode::PageUp => {
+                y = 0;
+            }
+            KeyCode::PageDown => {
+                y = height.saturating_sub(1);
+            }
+            KeyCode::Home => {
+                x = 0;
+            }
+            KeyCode::End => {
+                x = width.saturating_sub(1);
+            }
+            _ => (),
+        }
+        self.location = Location { x, y };
+        Ok(())
+    }
+    #[allow(clippy::needless_pass_by_value)]
+    fn evaluate_event(&mut self, event: Event) -> Result<(), Error> {
+        match event {
+            Event::Key(KeyEvent {
+                code,
+                kind: KeyEventKind::Press,
+                modifiers,
+                ..
+            }) => match (code, modifiers) {
+                (KeyCode::Char('q'), KeyModifiers::CONTROL) => {
+                    self.should_quit = true;
+                }
+                (
+                    KeyCode::Up
+                    | KeyCode::Down
+                    | KeyCode::Left
+                    | KeyCode::Right
+                    | KeyCode::PageDown
+                    | KeyCode::PageUp
+                    | KeyCode::End
+                    | KeyCode::Home,
+                    _,
+                ) => {
+                    self.move_point(code)?;
+                }
+                _ => {}
+            },
+            Event::Resize(width_u16, height_u16) => {
+                #[allow(clippy::as_conversions)]
+                let height = height_u16 as usize;
+
+                #[allow(clippy::as_conversions)]
+                let width = width_u16 as usize;
+                self.view.resize(Size { height, width });
+            }
+            _ => {}
+        }
+        Ok(())
+    }
+    fn refresh_screen(&mut self) -> Result<(), Error> {
+        Terminal::hide_caret()?;
+        Terminal::move_caret_to(Position::default())?;
+        if self.should_quit {
+            Terminal::clear_screen()?;
+            Terminal::print("Goodbye.\r\n")?;
+        } else {
+            self.view.render()?;
+            Terminal::move_caret_to(Position {
+                col: self.location.x,
+                row: self.location.y,
+            })?;
+        }
+
+        Terminal::show_caret()?;
+        Terminal::execute()?;
+        Ok(())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
+#![warn(clippy::all, clippy::pedantic, clippy::print_stdout)]
 mod editor;
 use editor::Editor;
 fn main() {
-    let editor = Editor::default();
-    editor.run();
+    Editor::default().run();
 }
